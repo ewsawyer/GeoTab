ccd3967a28960a19a359e19d4f043150a2de25b8
e-sawyer <109112191+ewsawyer@users.noreply.github.com>
2024-03-19 20:28:27 -0700

    Location api added

--- popup_script.js
+++ popup_script.js
@@ -2,3 +2,18 @@ const button = document.getElementById("btn-close");
 button.addEventListener("click", () => {
 	window.close();
 })
+
+const locationOutput = document.getElementById("location");
+
+const btnGetLocation = document.getElementById("btn-get-location");
+btnGetLocation.addEventListener("click", () => {
+	navigator.permissions.query({name: "geolocation"}).then(result => {
+		locationOutput.textContent = result.state;
+	})
+	
+	navigator.geolocation.getCurrentPosition(
+		loc => locationOutput.innerHTML = loc.coords.latitude + " " + loc.coords.longitude,
+		error => locationOutput.textContent = error.message,
+		{enableHighAccuracy: true}
+	)
+})
