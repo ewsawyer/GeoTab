327445c0609ba16a5b19010ed63f7c48223bfe39
J0yceNg <156155516+J0yceNg@users.noreply.github.com>
2024-04-02 19:32:32 -0700

    Removed btnGetLocation

--- popup_script.js
+++ popup_script.js
@@ -5,19 +5,6 @@ button.addEventListener("click", () => {
 
 const locationOutput = document.getElementById("location");
 
-const btnGetLocation = document.getElementById("btn-get-location");
-btnGetLocation.addEventListener("click", () => {
-	navigator.permissions.query({name: "geolocation"}).then(result => {
-		locationOutput.textContent = result.state;
-	})
-	
-	navigator.geolocation.getCurrentPosition(
-		loc => locationOutput.innerHTML = loc.coords.latitude + " " + loc.coords.longitude,
-		error => locationOutput.textContent = error.message,
-		{enableHighAccuracy: true}
-	)
-})
-
 document.getElementById('saveButton').addEventListener('click', function() {
     const url = document.getElementById('urlInput').value;
     if (!url) {
